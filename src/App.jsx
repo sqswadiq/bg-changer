d217ec3c8b62a06f5ef5ebb7be7fca037cc35bea
sqswadiq <sqswadique33@gmail.com>
2024-12-31 09:39:41 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,9 @@ function App() {
     <div className='d-flex flex-column align-items-center text-center' style={{  transition: 'background-color 0.4s ease-in-out',backgroundColor: bgcolor, height: '100vh',  paddingTop: '50px' }}>      
       <h1 style={{textShadow:'2px 2px 8px grey'}}>Background Color Changer</h1>
 
-      <form action="" className='d-flex justify-content-evenly flex-sm-column align-items-center  flex-md-row px-2' style={{width:'100%',marginTop:'50px'}}>
+      <form action="" className='d-flex justify-content-center flex-sm-column align-items-center  flex-md-row px-2' style={{width:'100%',marginTop:'50px'}}>
       <Button  onClick={()=>handleColor('yellow')} variant="outlined" style={{color:'black',borderColor:'black'}} className='mb-3'>Yellow</Button>
-      <Button  onClick={()=>handleColor('lime')} variant="outlined" style={{color:'black',borderColor:'black'}} className='mb-3'>green</Button>
+      <Button  onClick={()=>handleColor('lime')} variant="outlined" style={{color:'black',borderColor:'black'}} className='mb-3 mx-lg-5'>green</Button>
       <Button  onClick={()=>handleColor('orange')} variant="outlined" style={{color:'black',borderColor:'black'}} className='mb-3'>Orange</Button>
       </form>
 
